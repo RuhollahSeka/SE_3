bb7ef7a39d8e47c4a13bd03254bb364a66a96704
msi1 <r.sekaleshfar@gmail.com>
2020-04-25 00:10:56 +0430

    Handle creating a new participation

--- src/selab/mvc/controllers/AddStudentToCourseController.java
+++ src/selab/mvc/controllers/AddStudentToCourseController.java
@@ -2,15 +2,28 @@ package selab.mvc.controllers;
 
 import org.json.JSONObject;
 import selab.mvc.models.DataContext;
+import selab.mvc.models.DataSet;
+import selab.mvc.models.entities.Course;
+import selab.mvc.models.entities.Participation;
+import selab.mvc.models.entities.Student;
+import selab.mvc.views.JsonView;
 import selab.mvc.views.View;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 
 public class AddStudentToCourseController extends Controller {
 
+    private DataSet<Participation> participations;
+    private DataSet<Student> students;
+    private DataSet<Course> courses;
+
     public AddStudentToCourseController(DataContext dataContext) {
         super(dataContext);
+        this.participations = dataContext.getParticipations();
+        this.students = dataContext.getStudents();
+        this.courses = dataContext.getCourses();
     }
 
     @Override
@@ -23,7 +36,29 @@ public class AddStudentToCourseController extends Controller {
         String courseNo = input.getString("courseNo");
         String points = input.getString("points");
 
-        // TODO: Add required codes to associate the student with course
-        return null;
+        HashMap<String, String> result = new HashMap<>();
+        String participationNo = "St" + studentNo + "Co" + courseNo;
+        Participation participation = this.participations.get(participationNo);
+        if (participation != null)
+        {
+            result.put("success", "false");
+            return new JsonView(new JSONObject(result));
+        }
+
+        Student student = this.students.get(studentNo);
+        Course course = this.courses.get(courseNo);
+
+        participation = new Participation();
+        participation.setStudent(student);
+        participation.setCourse(course);
+        participation.setPoints(Integer.parseInt(points));
+
+        student.addParticipation(participation);
+        course.addParticipation(participation);
+
+        this.participations.add(participation);
+
+        result.put("success", "true");
+        return new JsonView(new JSONObject(result));
     }
 }
